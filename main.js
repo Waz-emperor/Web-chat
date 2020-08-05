35f13037cd71fe9c6078afdaa50013ec555e0141
Waz-emperor <64282899+Waz-emperor@users.noreply.github.com>
2020-08-05 19:57:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 async function getMessegesFromServer() {
 
 var response = await fetch  ('https://fchatiavi.herokuapp.com/get/null/?offset=0&limit=10');
-response = response.json();
+response = await response.json();
 console.log(response);
 }
