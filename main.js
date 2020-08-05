abbe63944c15b3cf4a9a40865edf2b4b4fd5ddc0
Waz-emperor <64282899+Waz-emperor@users.noreply.github.com>
2020-08-06 00:15:26 +0300

    vmc

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 
 
+
 var massages = document.getElementById ('messages');
+
+var sendButton = document.getElementById('send-btn');
+
+sendButton.addEventListener('click', sendUserMessage)
 
 async function getMessagesFromServer() {
 
-var response = await fetch('https://fchatiavi.herokuapp.com/get/tfge/?offset=0&limit=100');
+var response = await fetch('https://fchatiavi.herokuapp.com/get/tfge/?offset=0&limit=10');
 
 response = await response.json();
 
+var allMessagesHTML = '';
 for (var i = 0; i < response.length; i++) {
 var messegeData = response[i];
 console.log (massageData);
@@ -19,16 +25,41 @@ var message =
         <div class="message-text"> ${massageData.Message} </div>
     </div>`
     ;
-  messages.innerHTML = message;
+
+ allMessagesHTML = allMessagesHTML + message;
 
 
 
-  
 }
 
 
 
 
+messages.innerHTML =allMessagesHTML;
 
+}
 
+async function sendUserMessage() {
+
+var userNickname =  document.getElementById('nickname-input').value;
+var userMessage =  document.getElementById('Message-input').value;
+
+if (userNickname.length === 0) {
+  alert('ты должен ввести имя');
+  return;
+}
+
+if (userMessage.length === 0) {
+  alert('ты должен ввести сообщение');
+  return;
+}
+
+await fetch('https://fchatiavi.herokuapp.com/send/tfge/?offset=0&limit=10',{
+  method: 'POST',
+  body: JSON.sringify({
+    Name: userNickname,
+    Massage: userMessage,
+  })
+});
+  getMessagesFromServer();
 }
