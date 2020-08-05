c2a070081a8293f07ff27804a18d9c036592f456
Waz-emperor <64282899+Waz-emperor@users.noreply.github.com>
2020-08-05 20:41:28 +0300

    mc

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+
+
+var messeges = document.getElementById ('messeges');
+
 async function getMessegesFromServer() {
 
 var response = await fetch  ('https://fchatiavi.herokuapp.com/get/null/?offset=0&limit=10');
 response = await response.json();
-console.log(response);
+
+var message =
+
+    `<div class="masage">
+      <div class="masage-nickname">Name</div>
+        <div class="masage-text"> Messege </div>
+    </div>`;
+
+    messeges.innerHTML = messege;
+
+
 }
